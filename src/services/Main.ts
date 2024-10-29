ad5253b5102c75a1f49ca8d6b495d8abc4922869
AkashPotterHead <akasht3rdfeb@gmail.com>
2024-10-29 20:03:50 +0530

    Added mechanism to prompt the user for inputs in a cycle till the user wishes to exit the calculator application

--- src/services/Main.ts
+++ src/services/Main.ts
@@ -1,8 +1,37 @@
 // src/main.ts
 
 import { MathService } from "./MathService";
+import * as readline from 'readline';
 
-const mathService = new MathService();
-const result = mathService.add("3,5");
 
-console.log(`The sum of 3 and 5 is: ${result}`);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  // Helper function to wrap rl.question in a promise
+  const askQuestion = (query: string): Promise<string> => {
+    return new Promise(resolve => rl.question(query, resolve));
+  };
+
+  function exit() {
+    console.log('Exiting...');
+    rl.close();
+    process.exit();
+  }
+  
+  async function readAndCalculate() {
+    const numbers = await askQuestion('Enter the numbers as a comma separated string. Hit enter to exit. \n');
+    if(!numbers) exit ();
+    const operation = await askQuestion('Enter the operation to be performed(+, - ,*, /). Hit enter to exit.\n');
+    if(!operation) exit();
+    
+    const result = new MathService().init(numbers,operation);
+     console.log('\nResult = ',result,'\n')
+    readAndCalculate();
+  };
+
+  ( async function startCycle() {
+    await readAndCalculate();
+  })();
+
