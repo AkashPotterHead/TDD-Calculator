327aeb4255c4ec4306ab594b37a455f89a5bdfb4
AkashPotterHead <akasht3rdfeb@gmail.com>
2024-10-29 20:04:22 +0530

    Update MathService.ts
    Added basic functions for arithmetic operations

--- src/services/MathService.ts
+++ src/services/MathService.ts
@@ -1,11 +1,40 @@
 // src/services/MathService.ts
 
 export class MathService {
+
+  init(numbers: string, operation: string): number {
+    switch (operation) {
+      case '+': return this.add(numbers);
+      case '-': return this.subtract(numbers);
+      case '*': return this.multiply(numbers);
+      default: return 0;
+    }
+  }
+
     add(numbers: string): number {
       return numbers
         .split(",")                      // Split the string by commas
         .map(num => parseFloat(num))      // Convert each part to a number
         .reduce((sum, num) => sum + num, 0); // Sum up all the numbers
+    }
+
+    subtract(numbers: string): number {
+  
+      console.log("Still constructing");
+      return 0;
+      
+    }
+
+    multiply(numbers: string): number {
+      return numbers
+        .split(",")                      // Split the string by commas
+        .map(num => parseFloat(num))      // Convert each part to a number
+        .reduce((sum, num) => sum * num, 1); // Sum up all the numbers
+    }
+    
+    divide(numbers: string): number {
+      console.log("Still constructing");
+      return 0;
     }
   }
   
